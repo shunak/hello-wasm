159dbd0c6535f117baf91cfdb71281391f4084c0
shunak <shuna966@gmail.com>
2021-05-29 18:34:21 +0900

    deleted #[wasm-bindgen]

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,10 @@ extern crate wasm_bindgen;
 
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
 extern {
     pub fn alert(s: &str);
 }
 
-#[wasm_bindgen]
 pub fn greet(name: &str){
     alert(&format!("Hello, {}!",name));
 }
